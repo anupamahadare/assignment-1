633c8462afde96c2b0f05625f1e587dd9da179bc
anupamahadare <92136714+anupamahadare@users.noreply.github.com>
2021-10-16 12:19:25 +0530

    Q3  :  In a small company, the average salary of 3 employee is  Rs.:1000 per week. If 1 employee earns Rs.:1100  and other earns rs.:500 how much will 3rd employee earn?

---
+++ assignment1_q3.py
@@ -0,0 +1,12 @@
+#Q3 in a small company the avg salary of 3 emp is 
+#rs.:1000 per week if 1 emp earns rs.:1100 
+#anothers one rs.:500 how much will 3rd emp earn? 
+
+emp1_sal = 1100
+emp2_sal = 500
+emp3_sal = 0
+avg_sal = 1000
+
+a=emp1_sal+emp2_sal+emp3_sal
+x=(avg_sal*3)-a
+print(x)
